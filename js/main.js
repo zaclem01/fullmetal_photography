797623c448cd81b2a4e8f594caa787122f3b13f3
Zac Clemans <zaclem01@gmail.com>
2017-05-03 22:21:47 -0400

    add responsive styles, add setmore booking integration, add pixieset navigation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,39 @@
 $(document).ready(function() {
     var $topbar = $('.topbar');
+    var $menu = $('.topbar_menuIcon');
     var $navbar = $('.navigationBar');
     
     var navOffset = $navbar.offset().top;
+    var topbarHeight = $topbar.outerHeight();
 
     $(window).on('scroll', function(e) {
         if ($(this).scrollTop() > navOffset) {
             $navbar.toggleClass('navigationBar--fixed', true);
-            // $topbar.toggleClass('u-hidden', true);
         } else {
             $navbar.toggleClass('navigationBar--fixed', false);
-            // $topbar.toggleClass('u-hidden', false);
+        }
+    });
+
+    $(window).on('resize', function() {
+        navOffset = $navbar.offset().top;
+        if ($(this).width() >= 1024) {
+            $navbar.css('display', 'flex');
+        } else {
+            $navbar.css('display', 'none');
+        }
+    });
+
+    $menu.on('click', function(e) {
+        e.stopPropagation();
+        $navbar.css('display', 'flex');
+    });
+
+    $('.navigationBar_navLink, body').on('click', function() {
+        var width = $(window).width();
+        var smallRes = width <= 1007;
+
+        if (smallRes) {
+            $navbar.css('display', 'none');
         }
     });
 });
